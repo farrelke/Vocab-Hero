1adbb80418cc40bdd138a6047940783761a4a422
Kevin Farrell <farrelke@tcd.ie>
2019-07-31 13:07:33 +0800

    Added test stats

--- src/newTab/TestPage/TestPage.tsx
+++ src/newTab/TestPage/TestPage.tsx
@@ -17,36 +17,42 @@ class TestPage extends PureComponent<Props> {
   state = {
     vocabIndex: 0,
     vocabWords: null as VocabWord[],
-    showAnswer: false
+    showAnswer: false,
+    answered: 0,
+    correctAnswers: 0
   };
 
   async componentDidMount() {
     const vocabWords = await getVocabWords();
     const vocabIndex = Math.floor(Math.random() * vocabWords.length);
-    this.setState({ vocabWords, vocabIndex  });
+    this.setState({ vocabWords, vocabIndex });
   }
 
-  async showAnswer() {
+  showAnswer = async () => {
     this.setState({ showAnswer: true });
-  }
+  };
 
-  async next() {
-    const { vocabIndex, vocabWords } = this.state;
+  next = async (correct?: boolean) => {
+    const { vocabIndex, vocabWords, answered, correctAnswers } = this.state;
     this.setState({
       showAnswer: false,
-      vocabIndex: (vocabIndex + 1) % vocabWords.length
+      vocabIndex: (vocabIndex + 1) % vocabWords.length,
+      correctAnswers: correctAnswers + (correct ? 1 : 0),
+      answered: answered + 1
     });
-  }
+  };
 
   render() {
-    const { vocabWords, vocabIndex, showAnswer } = this.state;
+    const { vocabWords, vocabIndex, showAnswer, answered, correctAnswers } = this.state;
     if (!vocabWords) return null;
     const vocab = vocabWords[vocabIndex];
     if (!vocab) return null;
 
     return (
       <div className="TestPage">
-        <div className="TestPage__reading">{vocab.reading}</div>
+        <div className="TestPage__reading">
+          {showAnswer ? vocab.reading : ""}
+        </div>
         <div className="TestPage__word" onClick={() => speak(vocab.word)}>
           {vocab.word}
         </div>
@@ -61,14 +67,25 @@ class TestPage extends PureComponent<Props> {
           {showAnswer ? vocab.meaning : ""}
         </div>
 
-        <div
-          className="TestPage__showBtn"
-          onClick={() => (showAnswer ? this.next() : this.showAnswer())}
-        >
-          {showAnswer ? "Next Word" : "Show Meaning"}
-        </div>
+        {!showAnswer && (
+          <div className="TestPage__showBtn" onClick={this.showAnswer}>
+            Show
+          </div>
+        )}
+
+        {showAnswer && (
+          <div className="TestPage__buttons" >
+            <div className="TestPage__showBtn TestPage__showBtn--wrong" onClick={() => this.next(false)} >
+              Forgot
+            </div>
+            <div className="TestPage__showBtn TestPage__showBtn--correct" onClick={() => this.next(true)} >
+              Remembered
+            </div>
+          </div>
+        )}
 
 
+        <span style={{ marginTop: 30 }} >{correctAnswers} / {answered} Remembered Correctly</span>
 
         <div
           className="TestPage__closeBtn"
